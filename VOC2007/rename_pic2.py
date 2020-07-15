13d1c4898c7c8b9db96da1c009f4706e462dc9dc
hx358031364 <741965852@qq.com>
2020-07-15 14:42:05 +0800

    新增增加前缀名字

--- VOC2007/rename_pic2.py
+++ VOC2007/rename_pic2.py
@@ -1,23 +1,36 @@
+'''
+1.加前缀，不改原来的名字/
+2.只在后面加个.jpg后缀
+'''
 import os
-jpg_path = "E:\\改格式"
-jpg_filelist = os.listdir(jpg_path) #该文件夹下所有的文件（包括文件夹）
-count=0
+jpg_path = "D:\\data\\tv_source_data\\cctv\\cctv11\\"
+jpg_filelist = os.listdir(jpg_path) #该文件夹下所有的文件
 
-def get_all_files(dir):
-    files_ = []
-    list = os.listdir(dir)
-    for i in range(0, len(list)):
-        path = os.path.join(dir, list[i])
-        if os.path.isdir(path):
-            files_.extend(get_all_files(path))
-        if os.path.isfile(path):
-            files_.append(path)
-    return files_
+for jpg_file in jpg_filelist:   #遍历所有文件
+    jpg_Olddir=os.path.join(jpg_path,jpg_file)   #原来的文件路径
+    if os.path.isdir(jpg_Olddir):   #如果是文件夹则跳过
+        continue
+    jpg_Newdir = os.path.join(jpg_path, "10_" + jpg_file)  # 用字符串函数zfill 以0补全所需位数
+    os.rename(jpg_Olddir, jpg_Newdir)  # 重命名
 
 
-files = get_all_files(jpg_path)
-print(files)
 
-for f in files:
-    os.rename(f,f+str(count)+".jpg")
-    count+=1
+# count = 0
+# def get_all_files(dir):
+#     files_ = []
+#     list = os.listdir(dir)
+#     for i in range(0, len(list)):
+#         path = os.path.join(dir, list[i])
+#         if os.path.isdir(path):
+#             files_.extend(get_all_files(path))
+#         if os.path.isfile(path):
+#             files_.append(path)
+#     return files_
+#
+#
+# files = get_all_files(jpg_path)
+# print(files)
+#
+# for f in files:
+#     os.rename(f,f+str(count)+".jpg")
+#     count+=1
