c195b9a37196f45581f40c0cfccbe167353dc724
hx358031364 <741965852@qq.com>
2022-08-12 18:25:31 +0800

    voc与yolo互转

---
+++ VOC2007/voc2yolo.py
@@ -0,0 +1,82 @@
+import xml.etree.ElementTree as ET
+import pickle
+import os
+from os import listdir, getcwd
+from os.path import join
+import numpy as np
+import shutil
+import cv2
+
+with open('./label', 'r') as f:
+    names = f.read().split('\n')
+classes = list(filter(None, names))
+def convert(size, box):
+    l =  []
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[1]) / 2.0
+    y = (box[2] + box[3]) / 2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    l = [x, y, w, h]
+    l = np.array(l, dtype=np.float32)
+    assert (l >= 0).all(), 'negative labels'
+    assert (l <= 1).all(), 'non-normalized or out of bounds coordinate labels'
+    return (x, y, w, h)
+
+
+def convert_annotation(image_id):
+    img = cv2.imread(os.path.join(r'C:\Users\rmzk\Desktop\yilake\jpg', image_id + '.jpg'))
+    h, w, _ = img.shape
+    in_file = open(r'C:\Users\rmzk\Desktop\yilake/xml/%s.xml' % (image_id))
+    out_file = open(r'/data2/yolov5_logodet/datasets/data_logo_txt/%s.txt' % (image_id), 'w')
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult) == 1:
+            print(cls)
+            continue
+        cls_id = classes.index(cls)
+        try:
+            xmlbox = obj.find('bndbox')
+            # b = (float(xmlbox.find('xmin').text) if float(xmlbox.find('xmin').text) >= 0 else 0,
+            #      float(xmlbox.find('xmax').text) if float(xmlbox.find('xmax').text) <= w else w,
+            #      float(xmlbox.find('ymin').text) if float(xmlbox.find('ymin').text) >= 0 else 0,
+            #      float(xmlbox.find('ymax').text) if float(xmlbox.find('ymax').text) <= h else h)
+            b = (float(xmlbox.find('xmin').text),
+                 float(xmlbox.find('xmax').text),
+                 float(xmlbox.find('ymin').text),
+                 float(xmlbox.find('ymax').text))
+            bb = convert((w, h), b)
+            out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+        except Exception as e:
+            print(e)
+            print(image_id)
+            in_file.close()
+            shutil.move(
+                os.path.join(r'D:\data\logo\shumei\qingxi\img', image_id + '.jpg'),
+                r'D:\data\logo\shumei\qingxi\error')
+            shutil.move(
+                os.path.join(r'D:\data\logo\shumei\qingxi\xml', image_id + '.xml'),
+                r'D:\data\logo\shumei\qingxi\error')
+    in_file.close()
+    out_file.close()
+
+
+
+jpg_path = r'C:\Users\rmzk\Desktop\yilake\jpg'
+image_ids = os.listdir(jpg_path)
+
+for image in image_ids:
+    image_id = image[:-4]
+    convert_annotation(image_id)
---
+++ VOC2007/xml_check.py
@@ -0,0 +1,100 @@
+# coding=utf-8
+import os
+import xml.dom.minidom
+import cv2
+from collections import Counter
+import shutil
+# jpg对应文件夹
+jpg_path = r'C:\Users\rmzk\Desktop\majiang\jpg'
+# xml文件路径
+path = r'C:\Users\rmzk\Desktop\majiang\xml'
+jpgs = os.listdir(jpg_path)
+
+xmls = os.listdir(path)
+if len(jpgs) != len(xmls):
+    print('jpg与xml不对应')
+
+
+label = []
+
+with open('./label', 'r') as f:
+    names = f.read().split('\n')
+names_list = list(filter(None, names))
+
+for jpg_name in jpgs:  # 遍历文件夹
+    img_bool = False
+    xml_bool = False
+    # 得到一个xml完整的路径
+    xml_name = jpg_name.split(".")[0]
+    xml_name = xml_name+'.xml'
+    xml_path = os.path.join(path, xml_name)
+    # 读取xml
+    # dom = xml.dom.minidom.parse(xml_path)
+    f = open(xml_path, "r", encoding='utf-8')
+    r = f.read()
+    text = str(r.encode('utf-8'), encoding="utf-8")
+    # print(text)
+    # 使用minidom解析器打开 XML 文档
+    dom = xml.dom.minidom.parseString(text)
+    old_xml = dom.documentElement
+    root = dom.documentElement
+    object_root = root.getElementsByTagName('object')
+    length = len(object_root)
+    # print(length)
+    if length == 0:
+        print(xml_name, ' is not object')
+        xml_bool = True
+    else:
+        for root_i in range(length):
+            now_name_list = object_root[root_i].getElementsByTagName('name')[0].childNodes
+            if len(now_name_list) == 0:
+                print(xml_name, 'name is none')
+                xml_bool = True
+            # 如果name没在预设标签中，保存错误label名称
+            elif now_name_list[0].nodeValue not in names_list:
+                try:
+                    label.append(now_name_list[0].nodeValue)
+                except Exception as e :
+                    print(jpg_name)
+                xml_bool = True
+            # 查看坐标是否有问题
+            xmlbox = object_root[root_i].getElementsByTagName('bndbox')
+            xmin = float(xmlbox[0].getElementsByTagName('xmin')[0].childNodes[0].nodeValue)
+            xmax = float(xmlbox[0].getElementsByTagName('xmax')[0].childNodes[0].nodeValue)
+            ymin = float(xmlbox[0].getElementsByTagName('ymin')[0].childNodes[0].nodeValue)
+            ymax = float(xmlbox[0].getElementsByTagName('ymax')[0].childNodes[0].nodeValue)
+            if xmax - xmin < 10 or ymax - ymin < 10:
+                print(xml_name, 'box error')
+                xml_bool = True
+    try:
+        img = cv2.imread(os.path.join(jpg_path, jpg_name))
+        cv2.resize(img, (640, 640))
+    except Exception as e:
+        # print(e)
+        # print(jpg_path)
+        img_bool = True
+    if img is None:
+        img_bool = True
+        print(jpg_name, ' is none')
+    else:
+        h, w, _ = img.shape
+        size = old_xml.getElementsByTagName("size")
+        if size[0].getElementsByTagName("width")[0].childNodes[0].nodeValue != str(w):
+            print('xml_w!=img_w,img:', xml_name)
+            xml_bool = True
+        if size[0].getElementsByTagName("height")[0].childNodes[0].nodeValue != str(h):
+            print('xml_h!=img_h,img:', xml_name)
+            xml_bool = True
+    if img_bool or xml_bool:
+        f.close()
+        shutil.move(xml_path, r'C:\Users\rmzk\Desktop\sex_sample\sex_sample\none')
+        shutil.move(os.path.join(jpg_path, jpg_name), r'C:\Users\rmzk\Desktop\sex_sample\sex_sample\none')
+
+print(label)
+
+
+# 统计标签数量
+# count = Counter(label)
+# print(count)
+# 输出元素3的个数
+# print(count[3])
